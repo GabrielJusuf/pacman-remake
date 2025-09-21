2ba93970bb9e1537bace793e66f92ba933d7908a
Gabriel Jusuf <GabrielJusuf19@gmail.com>
2025-09-22 02:19:56 +1000

    Finished development of the level generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -15,6 +15,9 @@ public class LevelGenerator : MonoBehaviour
     public GameObject tJunction;        // 7
     public GameObject ghostExit;        // 8
 
+    private float tileSize = 1.0f;
+
+    // TO MARKER: Replace quad with another array for test case marking
     private int[,] quad = new int[,]
     {
         {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
@@ -34,14 +37,24 @@ public class LevelGenerator : MonoBehaviour
         {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
     };
 
+
     private int[,] full;
 
     void Start()
     {
         GameObject manualLevel = GameObject.Find("Level");
         Destroy(manualLevel);
-
         full = BuildFullMap(quad);
+
+        var prevGen = GameObject.Find("GeneratedLevel");
+        if (prevGen) Destroy(prevGen);
+        var newLevel = new GameObject("GeneratedLevel").transform;
+
+        var cam = Camera.main;
+        var camPos = cam.transform.position;
+        newLevel.position = new Vector3(camPos.x, camPos.y, 0f);
+
+        Generate(full, newLevel);
     }
 
     int[,] BuildFullMap(int[,] quad)
@@ -84,14 +97,33 @@ public class LevelGenerator : MonoBehaviour
         return fullMap;
     }
 
-    void Generate(int[,] map)
+    void Generate(int[,] map, Transform parent)
     {
+        int rowSize = map.GetLength(0);
+        int colSize = map.GetLength(1);
+
+        for (int row = 0; row < rowSize; row++)
+        {
+            for (int col = 0; col < colSize; col++)
+            {
+                int id = map[row, col];
+                GameObject prefab = GetPrefab(id);
+                if (!prefab) continue;
+
+                Vector3 pos = GridToWorldCentered(col, row, colSize, rowSize);
+                var tile = Instantiate(prefab, pos, Quaternion.identity, parent);
+                tile.transform.rotation = FindRotation(id, map, row, col);
+            }
+        }
     }
 
-    Vector3 GridToWorld(int col, int row)
+    Vector3 GridToWorldCentered(int col, int row, int cols, int rows)
     {
-        float x = col * 1f;
-        float y = -row * 1f;
+        float cx = (cols - 1) * 0.5f;
+        float cy = (rows - 1) * 0.5f;
+
+        float x = (col - cx) * tileSize;
+        float y = -(row - cy) * tileSize;
         return new Vector3(x, y, 0f);
     }
 
@@ -123,23 +155,78 @@ public class LevelGenerator : MonoBehaviour
         int left = GetID(map, row, col-1);
         int right = GetID(map, row, col+1);
 
-        //
+        // Get wall status for neighbouring tiles
         bool wU = isWallLike(up);
         bool wD = isWallLike(down);
         bool wL = isWallLike(left);
-        bool wR = isWallLike(right)
+        bool wR = isWallLike(right);
+
+        // Get Straight wall status for neighbouring tiles
+        bool sU = isStraightWall(up);
+        bool sD = isStraightWall(down);
+        bool sL = isStraightWall(left);
+        bool sR = isStraightWall(right);
+
+        // Case: Straight walls
+        if (id == 2 || id == 4 || id == 8)
+        {
+            if ((wL && wR) && !(wU && wD)) return Quaternion.identity;
+            if ((wU && wD) && !(wL && wR)) return Quaternion.Euler(0, 0, 90f);
+            if (wL || wR) return Quaternion.identity;
+            if (wU || wD) return Quaternion.Euler(0, 0, 90f);
+            return Quaternion.identity;
+        }
+
+        // Case: Corner walls
+        if (id == 1 || id == 3)
+        {
+            // Check for adjacent straight walls that extend more than 1 tile
+            bool isLinkedU = (sU && isStraightWall(GetID(map, row-2, col)));
+            bool isLinkedD = (sD && isStraightWall(GetID(map, row+2, col)));
+            bool isLinkedL = (sL && isStraightWall(GetID(map, row, col-2)));
+            bool isLinkedR = (sR && isStraightWall(GetID(map, row, col+2)));
+
+            // Case 1: Only 2 neighbouring walls
+            if ((wR && wD) && !(wL && wU)) return Quaternion.identity;
+            if ((wL && wD) && !(wR && wU)) return Quaternion.Euler(0, 0, 270f);
+            if ((wL && wU) && !(wR && wD)) return Quaternion.Euler(0, 0, 180f);
+            if ((wR && wU) && !(wL && wD)) return Quaternion.Euler(0, 0, 90f);
+
+            // Case 2: Connected to 2 straight walls
+            if (isLinkedR && isLinkedD) return Quaternion.identity;
+            if (isLinkedL && isLinkedD) return Quaternion.Euler(0, 0, 270f);
+            if (isLinkedL && isLinkedU) return Quaternion.Euler(0, 0, 180f);
+            if (isLinkedR && isLinkedU) return Quaternion.Euler(0, 0, 90f);
+            return Quaternion.identity;
+        }
+
+        // Case T Junction
+        if (id == 7)
+        {
+            if (!wU && wD && wL && wR) return Quaternion.identity;
+            if (!wR && wD && wU && wL) return Quaternion.Euler(0,0, 90f);
+            if (!wD && wU && wL && wR) return Quaternion.Euler(0, 0, 180f);
+            if (!wL && wD && wU && wR) return Quaternion.Euler(0, 0, 270f);
+            return Quaternion.identity;
+        }
+        return Quaternion.identity;
     }
 
     //Helpers
 
     bool isWallLike(int id)
     {
-        return id == 1 || id == 2 || id == 3 || id == 4 || id == 7 || id == 8
+        return id == 1 || id == 2 || id == 3 || id == 4 || id == 7 || id == 8;
+    }
+
+    bool isStraightWall(int id)
+    {
+        return id == 2 || id == 4 || id == 8;
     }
 
     int GetID(int[,] map, int row, int col)
     {
-        if (row < 0 || c < 0 || row >= map.GetLength(0) || c >= map.GetLength(1)) return 0;
+        if (row < 0 || col < 0 || row >= map.GetLength(0) || col >= map.GetLength(1)) return 0;
         return map[row, col];
     }
 
